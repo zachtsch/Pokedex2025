54d5ec68223615e5ab28b67334728227e86d1b77
Sara Reifenrath <wisbadger88@gmail.com>
2025-03-24 12:36:01 -0500

    Fixed minor issue with background not filling larger screen resolutions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,18 @@
-import { useRouter } from 'expo-router';
+import { useRouter } from "expo-router";
 import React, { useState, useEffect } from "react";
-import { Dimensions, Platform, View, Text, Image, FlatList, StyleSheet, Pressable, ImageBackground } from "react-native";
-import { plateMap, typeFormat } from '../about';
-import * as Font from 'expo-font';
+import {
+  Dimensions,
+  Platform,
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  Pressable,
+  ImageBackground,
+} from "react-native";
+import { plateMap, typeFormat } from "../about";
+import * as Font from "expo-font";
 
 // Given a type (string) return the color hex number associate with type
 export function findBackgroundColor(type: string): string {
@@ -29,53 +39,49 @@ export function findBackgroundColor(type: string): string {
   return typeColors[type];
 }
 
-export default function PokemonList(){
+export default function PokemonList() {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
   const [loading, setLoading] = useState(false);
   const [nextPage, setNextPage] = useState(1);
   const router = useRouter();
 
-
   type pokeType = {
-    type : {
-      name : string
-    }
-  }
+    type: {
+      name: string;
+    };
+  };
 
   interface Pokemon {
-    name : string;
-    id : string
+    name: string;
+    id: string;
     sprites: {
       front_default: string;
     };
-    types : pokeType[]
+    types: pokeType[];
   }
 
-
   // Appends the json of each pokemon, in the range startId-endId, to pokemonList
-  async function fetchPokemon(startId : number, endId : number){
+  async function fetchPokemon(startId: number, endId: number) {
     setLoading(true);
-    const newPokemon : Pokemon[] = [];
+    const newPokemon: Pokemon[] = [];
     for (let id = startId; id <= endId && id <= 1025; id++) {
       try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const data : Pokemon = await response.json();
+        const data: Pokemon = await response.json();
         newPokemon.push(data);
       } catch (error) {
         console.error("Error fetching Pokémon:", error);
       }
     }
-    setPokemonList((prev : Pokemon[]) => [...prev, ...newPokemon]);
+    setPokemonList((prev: Pokemon[]) => [...prev, ...newPokemon]);
     setLoading(false);
-  };
-
+  }
 
   // Initial fetch (first 20 Pokémon)
   useEffect(() => {
     fetchPokemon(nextPage, nextPage + 19);
     setNextPage(nextPage + 20);
   }, []);
-
 
   // Load more Pokémon when the user scrolls to the end
   const loadMorePokemon = () => {
@@ -85,7 +91,7 @@ export default function PokemonList(){
     }
   };
 
-  // if pokemon has a second type, return the hex number of the color associate with type. 
+  // if pokemon has a second type, return the hex number of the color associate with type.
   //      otherwise, return the color of the first type.
   function findSecondBackground(types: Array<pokeType>) {
     if (types.length == 1) {
@@ -96,55 +102,67 @@ export default function PokemonList(){
   }
 
   // redirects to about component when pokemon is touched/clicked
-  function touch(id : string){
+  function touch(id: string) {
     router.push({
-      pathname: '/about',
-      params: { query: `${id}`},
+      pathname: "/about",
+      params: { query: `${id}` },
     });
   }
-  
 
   // Render each Pokémon item
-  const renderPokemon = ({ item } : {item : Pokemon}) => (
-    <Pressable style={styles.dataContainer} onPress={()=>touch(item.id)}>
-        <View style={[styles.mybackground,{borderLeftColor: findBackgroundColor(item.types[0].type.name), borderBottomColor: findSecondBackground(item.types)}]}></View>
-        <Text style={styles.id}>#{String(item.id).padStart(4, '0')}</Text>
-        <View style={styles.descriptionContainer}>
-          <Image
-            source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.id}.png` }}
-            style={styles.image}
-          />
-          <Text style={styles.name}>{item.name}</Text>
-          <Text style={styles.type}>
-            {typeFormat(item.types).map((plate) => {
-                return (<Image key={plate} source={plateMap[plate]}/>)
-              })}
-          </Text>
-        </View>
+  const renderPokemon = ({ item }: { item: Pokemon }) => (
+    <Pressable style={styles.dataContainer} onPress={() => touch(item.id)}>
+      <View
+        style={[
+          styles.mybackground,
+          {
+            borderLeftColor: findBackgroundColor(item.types[0].type.name),
+            borderBottomColor: findSecondBackground(item.types),
+          },
+        ]}
+      ></View>
+      <Text style={styles.id}>#{String(item.id).padStart(4, "0")}</Text>
+      <View style={styles.descriptionContainer}>
+        <Image
+          source={{
+            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.id}.png`,
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.type}>
+          {typeFormat(item.types).map((plate) => {
+            return <Image key={plate} source={plateMap[plate]} />;
+          })}
+        </Text>
+      </View>
     </Pressable>
   );
 
-    // Fit to mobile devices 
-    let numColumns = 3; //first assume it's desktop
-    let isWeb = false;
-    if (Platform.OS === 'web') isWeb = true;
+  // Fit to mobile devices
+  let numColumns = 3; //first assume it's desktop
+  let isWeb = false;
+  if (Platform.OS === "web") isWeb = true;
 
-    if (isWeb){
-      let windowWidth = Dimensions.get('window').width;
-      if (windowWidth < 750) numColumns = 1; //mobile web
-    } else {
-      numColumns = 1; //ios or android
-    }
+  if (isWeb) {
+    let windowWidth = Dimensions.get("window").width;
+    if (windowWidth < 750) numColumns = 1; //mobile web
+  } else {
+    numColumns = 1; //ios or android
+  }
 
-    // grab font
-    useEffect(() => {
-      Font.loadAsync({
-        'pokeFontMain': require('../../assets/fonts/pokeFontMain.ttf'),
-      });
-    }, []);
+  // grab font
+  useEffect(() => {
+    Font.loadAsync({
+      pokeFontMain: require("../../assets/fonts/pokeFontMain.ttf"),
+    });
+  }, []);
 
   return (
-    <ImageBackground source={require('../../assets/images/pokeBack.png')} style={styles.pokeContainer}>
+    <ImageBackground
+      source={require("../../assets/images/pokeBack.png")}
+      style={styles.pokeContainer}
+    >
       <Text style={styles.title}>Pokédex ◓</Text>
       <FlatList
         numColumns={numColumns}
@@ -157,26 +175,28 @@ export default function PokemonList(){
       />
     </ImageBackground>
   );
-};
+}
 
 const styles = StyleSheet.create({
   title: {
-       fontSize: 80,
-       fontWeight: 'bold',
-       fontFamily: 'pokeFontMain'
+    fontSize: 80,
+    fontWeight: "bold",
+    fontFamily: "pokeFontMain",
   },
   pokeContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    height: "100%",
+    width: "100%",
   },
   descriptionContainer: {
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
-  mybackground:{
-    position: 'absolute',
+  mybackground: {
+    position: "absolute",
     borderRadius: 0,
     borderLeftWidth: 250,
     borderBottomWidth: 250,
@@ -194,24 +214,24 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
     padding: 0,
-    margin: 0
+    margin: 0,
   },
-  id:{
+  id: {
     fontSize: 24,
-    textAlign: 'left',
+    textAlign: "left",
     width: 210,
     paddingTop: 10,
-    fontFamily: 'pokeFontMain'
+    fontFamily: "pokeFontMain",
   },
-  name:{
-    textTransform: 'uppercase',
+  name: {
+    textTransform: "uppercase",
     fontSize: 28,
     fontWeight: "bold",
-    fontFamily: 'pokeFontMain'
+    fontFamily: "pokeFontMain",
   },
-  type:{
+  type: {
     fontSize: 28,
-    flexDirection: 'row',
-    gap: 5
-  }
+    flexDirection: "row",
+    gap: 5,
+  },
 });
